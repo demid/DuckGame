25b2e9889cc166e2370caf5c4bab5a5c79af1044
Alexey Chumak <alexey.chumak@nasctech.com>
2012-05-03 16:50:58 +0300

    properties variable in JCrossWayDialog  has changed to final static

--- RoadEditor/src/com/editor/screen/dialog/JCrossWayDialog.java
+++ RoadEditor/src/com/editor/screen/dialog/JCrossWayDialog.java
@@ -17,19 +17,17 @@ import java.awt.event.KeyEvent;
  */
 public class JCrossWayDialog extends JDialog {
     //Settings from properties file
-    private int dialogWidth = Properties.getInt(Properties.Settings.JCW_DIALOG_WIDTH);
-    private int dialogBorderWidth = Properties.getInt(Properties.Settings.JCW_DIALOG_BORDER_WIDTH);
+    private static final int DIALOG_WIDTH = Properties.getInt(Properties.Settings.JCW_DIALOG_WIDTH);
+    private static final int DIALOG_BORDER_WIDTH = Properties.getInt(Properties.Settings.JCW_DIALOG_BORDER_WIDTH);
+    private static final int DIALOG_HEIGHT = Properties.getInt(Properties.Settings.JCW_DIALOG_HEIGHT);
+    private static final double DIALOG_SPINNER_STEP = Properties.getDouble(Properties.Settings.JCW_DIALOG_SPINNER_STEP);
+    private static final int DIALOG_COMPONENT_WIDTH = Properties.getInt(Properties.Settings.JCW_DIALOG_COMPONENT_WIDTH);
+    private static final int DIALOG_COMPONENT_HEIGHT = Properties.getInt(Properties.Settings.JCW_DIALOG_COMPONENT_HEIGHT);
 
-    private int dialogHeight = Properties.getInt(Properties.Settings.JCW_DIALOG_HEIGHT);
-    private double dialogSpinnerStep = Properties.getDouble(Properties.Settings.JCW_DIALOG_SPINNER_STEP);
-    private int dialogComponentWidth = Properties.getInt(Properties.Settings.JCW_DIALOG_COMPONENT_WIDTH);
-    private int dialogComponentHeight = Properties.getInt(Properties.Settings.JCW_DIALOG_COMPONENT_HEIGHT);
-
-
-    private String dialogAngleLabel = Properties.getLabel(Properties.Labels.JCW_DIALOG_ANGLE_LABEL);
-    private String dialogScaleLabel = Properties.getLabel(Properties.Labels.JCW_DIALOG_SCALE_LABEL);
-    private String okButtonLabel = Properties.getLabel(Properties.Labels.OK_BUTTON_LABEL);
-    private String cancelButtonLabel = Properties.getLabel(Properties.Labels.CANCEL_BUTTON_LABEL);
+    private static final String DIALOG_ANGLE_LABEL = Properties.getLabel(Properties.Labels.JCW_DIALOG_ANGLE_LABEL);
+    private static final String DIALOG_SCALE_LABEL = Properties.getLabel(Properties.Labels.JCW_DIALOG_SCALE_LABEL);
+    private static final String OK_BUTTON_LABEL = Properties.getLabel(Properties.Labels.OK_BUTTON_LABEL);
+    private static final String CANCEL_BUTTON_LABEL = Properties.getLabel(Properties.Labels.CANCEL_BUTTON_LABEL);
 
 
     private JSpinner angleSpinner;
@@ -42,22 +40,22 @@ public class JCrossWayDialog extends JDialog {
     public JCrossWayDialog(String title, double angle, double scale) {
         setTitle(title);
         JPanel innerPanel = new JPanel(new VerticalBagLayout());
-        angleSpinner = new JSpinner(new SpinnerNumberModel(angle, -Double.MAX_VALUE, Double.MAX_VALUE, dialogSpinnerStep));
-        scaleSpinner = new JSpinner(new SpinnerNumberModel(scale, 0, Double.MAX_VALUE, dialogSpinnerStep));
-        JLabel angleLabel = new JLabel(dialogAngleLabel);
-        angleLabel.setPreferredSize(new Dimension(dialogComponentWidth,dialogComponentHeight));
+        angleSpinner = new JSpinner(new SpinnerNumberModel(angle, -Double.MAX_VALUE, Double.MAX_VALUE, DIALOG_SPINNER_STEP));
+        scaleSpinner = new JSpinner(new SpinnerNumberModel(scale, 0, Double.MAX_VALUE, DIALOG_SPINNER_STEP));
+        JLabel angleLabel = new JLabel(DIALOG_ANGLE_LABEL);
+        angleLabel.setPreferredSize(new Dimension(DIALOG_COMPONENT_WIDTH, DIALOG_COMPONENT_HEIGHT));
         innerPanel.add(angleLabel);
         innerPanel.add(angleSpinner);
-        JLabel scaleLabel = new JLabel(dialogScaleLabel);
-        scaleLabel.setPreferredSize(new Dimension(dialogComponentWidth,dialogComponentHeight));
+        JLabel scaleLabel = new JLabel(DIALOG_SCALE_LABEL);
+        scaleLabel.setPreferredSize(new Dimension(DIALOG_COMPONENT_WIDTH, DIALOG_COMPONENT_HEIGHT));
         innerPanel.add(scaleLabel);
         innerPanel.add(scaleSpinner);
-        JButton okButton = new JButton(okButtonLabel);
-        okButton.setPreferredSize(new Dimension(dialogComponentWidth,dialogComponentHeight));
+        JButton okButton = new JButton(OK_BUTTON_LABEL);
+        okButton.setPreferredSize(new Dimension(DIALOG_COMPONENT_WIDTH, DIALOG_COMPONENT_HEIGHT));
         okButton.addActionListener(okActionListener);
 
-        JButton cancelButton = new JButton(cancelButtonLabel);
-        cancelButton.setPreferredSize(new Dimension(dialogComponentWidth,dialogComponentHeight));
+        JButton cancelButton = new JButton(CANCEL_BUTTON_LABEL);
+        cancelButton.setPreferredSize(new Dimension(DIALOG_COMPONENT_WIDTH, DIALOG_COMPONENT_HEIGHT));
         cancelButton.addActionListener(cancelActionListener);
 
 
@@ -67,12 +65,12 @@ public class JCrossWayDialog extends JDialog {
         innerPanel.add(panel);
 
 
-        innerPanel.setBounds(dialogBorderWidth, dialogBorderWidth, dialogWidth - dialogBorderWidth*2,dialogHeight - dialogBorderWidth*2);
+        innerPanel.setBounds(DIALOG_BORDER_WIDTH, DIALOG_BORDER_WIDTH, DIALOG_WIDTH - DIALOG_BORDER_WIDTH * 2, DIALOG_HEIGHT - DIALOG_BORDER_WIDTH * 2);
 
         getContentPane().setLayout(null);
         getContentPane().add(innerPanel);
 
-        setSize(dialogWidth, dialogHeight);
+        setSize(DIALOG_WIDTH, DIALOG_HEIGHT);
         setResizable(false);
         getRootPane().setDefaultButton(okButton);
         getRootPane().getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT).put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "CANCEL");
